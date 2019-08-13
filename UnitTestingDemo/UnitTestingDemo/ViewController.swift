8da66bb072e7c23f97cd3f27e7944aa113518fc3
Kristijan Kralj <kristijan.kralj@live.com>
2019-08-13 21:02:36 +0200

    Initial commit

---
+++ UnitTestingDemo/UnitTestingDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestingDemo
+//
+//  Created by Kristijan Kralj on 13/08/2019.
+//  Copyright © 2019 Kristijan Kralj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
